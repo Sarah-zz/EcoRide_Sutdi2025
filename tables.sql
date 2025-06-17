a90dd2264daa73ce8b9d30d88988a33367ed0ac2
Sarah-zz <sarah.zogbi02@gmail.com>
2025-06-17 12:11:49 +0200

    MAJ tables SQL

--- tables.sql
+++ tables.sql
@@ -3,22 +3,23 @@ CREATE DATABASE ecoride_db;
 -- Structure de la table `users`
 
 CREATE TABLE `users` (
-  id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  pseudo varchar(50) NOT NULL,
-  first_name varchar(100) NOT NULL,
-  last_name varchar(100) NOT NULL,
-  email varchar(255) NOT NULL,
-  phone varchar(20) DEFAULT NULL,
-  password varchar(255) NOT NULL,
-  credits int DEFAULT '0',
-  profile_picture varchar(255) DEFAULT 'default_profile.png',
-  rating int DEFAULT '0'
+    id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    pseudo varchar(50) NOT NULL,
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    email varchar(255) NOT NULL UNIQUE,
+    phone varchar(20) DEFAULT NULL,
+    password varchar(255) NOT NULL,
+    credits int DEFAULT '0',
+    profile_picture varchar(255) DEFAULT 'default_profile.png',
+    rating int DEFAULT '0'
 )
 
 -------------
 INSERT INTO users (pseudo, first_name, last_name, email, phone, password, credits, profile_picture, rating) VALUES
-('JeanDupond', 'Jean', 'Dupond', 'jean.dupond@example.com', '0612345678', 'test', 15, 'profile1.png', 4)
+('JeanDupond', 'Jean', 'Dupond', 'jean.dupond@example.com', '0612345678', '$2y$10$E.VlV8k9w0X1z3y5u7i9o1p2a3s4d5f6g7h8j9k0l1m2n3b4v5c6x7z8a9b0c1d', 15, 'profile1.png', 4)
 
+/*      mdp : monSuperMotDePasse123!     */
 -------------
 CREATE TABLE `trajets` (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -32,7 +33,6 @@ CREATE TABLE `trajets` (
     places_disponibles INT NOT NULL,
     is_electric_car BOOLEAN NOT NULL DEFAULT FALSE, -- Vrai si voiture électrique
     description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_conducteur
         FOREIGN KEY (conducteur_id)
@@ -52,14 +52,14 @@ INSERT INTO trajets (
     is_electric_car,
     description
 ) VALUES (
-    1, -- Assurez-vous que cet ID de conducteur existe dans votre table 'users'
+    1,
     'Paris',
     'Marseille',
     '2025-06-20',
     '10:00:00',
     '16:30:00',
-    10, -- Le prix est maintenant un entier et ne dépasse pas 10
-    3, -- Nombre de places disponibles
-    FALSE, -- Mettez TRUE si c'est une voiture électrique
+    10,
+    3,
+    FALSE, -- TRUE si c'est une voiture électrique
     'Trajet direct et confortable. Pause déjeuner prévue à mi-chemin.'
 );
