d2490fa5da15aba7f8ee79eabf0050b9f8d688a1
Sarah-zz <sarah.zogbi02@gmail.com>
2025-06-23 11:00:06 +0200

    Feat : addMongoDb

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,35 @@
 # Utilisation d'une image PHP-FPM basée sur Alpine (légère)
 FROM php:8.2-fpm-alpine
 
-# Définir le répertoire de travail dans le conteneur
 WORKDIR /var/www/html
 
-# Installer les dépendances système nécessaires pour PHP et Composer
+# Installer les dépendances système nécessaires pour PHP et Composer et le driver MongoDB
 # Ex: pdo_mysql pour la connexion à la base de données
 RUN apk add --no-cache \
-    nginx \
+    autoconf \
+    g++ \
+    make \
+    pkgconfig \
+    curl-dev \
+    openssl-dev \
+    libxml2-dev \
+    libzip-dev \
     mysql-client \
     git \
     unzip \
-    && docker-php-ext-install pdo_mysql
+    && rm -rf /var/cache/apk/**
+
+RUN docker-php-ext-install pdo_mysql zip
+
+RUN pecl install mongodb-1.17.0 && \
+    docker-php-ext-enable mongodb
 
 # Installer Composer globalement
 COPY --from=composer/composer:latest /usr/bin/composer /usr/local/bin/composer
 
 # Copier votre code source dans le conteneur
-# .dockerignore peut être utile ici
 COPY . .
 
-# Installer les dépendances PHP via Composer (si elles n'ont pas déjà été installées sur l'hôte)
-# Optionnel : si vous voulez que Composer s'exécute à l'intérieur du conteneur
-# RUN composer install --no-dev --optimize-autoloader
 
 # Exposer le port PHP-FPM par défaut (9000)
 EXPOSE 9000
--- tables.sql
+++ tables.sql
@@ -1,6 +1,4 @@
 CREATE DATABASE ecoride_db;
---------------------------------------------------------
--- Structure de la table `users`
 
 CREATE TABLE `users` (
     id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -16,13 +14,6 @@ CREATE TABLE `users` (
     role int DEFAUT '1',
 );
 
--------------
-INSERT INTO users (pseudo, first_name, last_name, email, phone, password, credits, profile_picture, rating, role) VALUES
-('JeanDupond', 'Jean', 'Dupond', 'jean.dupond@example.com', '0612345678', '$2y$10$E.VlV8k9w0X1z3y5u7i9o1p2a3s4d5f6g7h8j9k0l1m2n3b4v5c6x7z8a9b0c1d', 15, 'profile1.png', 4, 1)
-/*      mdp : monSuperMotDePasse123!     */
-
-
--------------
 CREATE TABLE `trajets` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     conducteur_id INT NOT NULL,
@@ -42,29 +33,7 @@ CREATE TABLE `trajets` (
 );
 
 -------------
-INSERT INTO trajets (
-    conducteur_id,
-    ville_depart,
-    ville_arrivee,
-    date_trajet,
-    heure_depart,
-    heure_arrivee,
-    prix,
-    places_disponibles,
-    is_electric_car,
-    description
-) VALUES (
-    1,
-    'Paris',
-    'Marseille',
-    '2025-06-20',
-    '10:00:00',
-    '16:30:00',
-    10,
-    3,
-    FALSE, -- TRUE si c'est une voiture électrique
-    'Trajet direct et confortable. Pause déjeuner prévue à mi-chemin.'
-);
+
 
 --------------------------------------
 CREATE TABLE vehicles (
